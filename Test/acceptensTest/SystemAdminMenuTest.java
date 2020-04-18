012bdcf0501cb1f8577eb6e120fe6fa805e6f8b8
school & work <michalez@post.bgu.ac.il>
2020-04-18 19:01:59 +0300

    michal Admin test

--- Test/acceptensTest/SystemAdminMenuTest.java
+++ Test/acceptensTest/SystemAdminMenuTest.java
@@ -5,7 +5,9 @@ import BusniesServic.Business_Layer.UserManagement.Subscription;
 import BusniesServic.Business_Layer.UserManagement.SystemAdministrator;
 import BusniesServic.Enum.ActionStatus;
 import BusniesServic.Service_Layer.DataManagement;
+import Presentation_Layer.StartSystem;
 import Presentation_Layer.Users_Menu.GuestUserMenu;
+import Presentation_Layer.Users_Menu.SystemAdminUserMenu;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
@@ -18,35 +20,53 @@ import java.util.Collection;
 public class SystemAdminMenuTest {
 
     /**
-     * Test - GM1
+     * Test - AM1
      */
     @RunWith(Parameterized.class)
     public static class AdminMenuTest {
         //parameter
+        String number;
+        String arg1;
+        String arg2;
+        boolean ans;
 
         @Parameterized.Parameters
         public static Collection<Object[]> data() {
             return Arrays.asList(new Object[][]{
-                    {}
+                    //
+                    {"1","teamteam",null,false},// close non existing team
+                    {"1","teamOne",null,true} ,//close team
+                    //
+                    {"2","doli",null,false}, // user not exists
+                    {"2","del",null,true},// remove subscription permanently
+                    //
+                    {"3","1",null,true}, // see complaint
+                    {"3","2","",false}, // invalid response to complaint
+                    {"3","2","thank you for letting us know about the problem, we will fix it a.s.a.p :)",true}, // respond to complaint
+                    //
+                    {"4","1",null,true}, // watch log
             });
         }
 
-        public AdminMenuTest(){
-            //init parameters
+        public AdminMenuTest(String number, String arg1, String arg2,boolean ans){
+            this.number = number;
+            this.arg1 = arg1;
+            this.arg2 = arg2;
+            this.ans = ans;
         }
 
         @Test
         public void AdminTest1() {
-            Subscription g = new SystemAdministrator("Admin","123456","a@a.a");
-            DataManagement.setSubscription(g);
-            DataManagement.setCurrent(g);
+            if(number.equals("1")){
+                StartSystem sys = new StartSystem();
+                sys.startFromDB();
+            }
+            DataManagement.setCurrent(DataManagement.containSubscription("SysAdmin"));
+            DataManagement.setSubscription(DataManagement.containSubscription("SysAdmin"));
 
-            GuestUserMenu GM = new GuestUserMenu();
-            String[] args = new String[20];
-            //add strings to args!!!!!!
+            SystemAdminUserMenu AM = new SystemAdminUserMenu();
+            ActionStatus ac = AM.presentUserMenu(new String[]{number,arg1,arg2});
 
-
-            ActionStatus ac = GM.presentUserMenu(args);
             System.out.println(ac.isActionSuccessful() + " " +ac.getDescription());
 
 
