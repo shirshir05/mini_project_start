abd9b941f25d1897274f97731fa536968d450a30
shirshir05 <shir0@post.bgu.ac.il>
2020-04-18 13:08:20 +0300

    TeamUsersMenuTest

--- Test/acceptensTest/FanMenuTest.java
+++ Test/acceptensTest/FanMenuTest.java
@@ -1,5 +1,6 @@
 package acceptensTest;
 
+import BusniesServic.Business_Layer.Game.Game;
 import BusniesServic.Business_Layer.UserManagement.Fan;
 import BusniesServic.Business_Layer.UserManagement.Guest;
 import BusniesServic.Business_Layer.UserManagement.Subscription;
@@ -17,13 +18,14 @@ import java.util.Collection;
 
 @RunWith(Enclosed.class)
 public class FanMenuTest {
-
     /**
      * Test - FM1
      */
     @RunWith(Parameterized.class)
     public static class FanTest {
         //parameter
+
+
         String number;
         String arg1;
         String arg2;
@@ -34,8 +36,25 @@ public class FanMenuTest {
                     //3.1 Logout
                     {"1","shir","1"},// =>password incorrect
                     {"1","shir","12345"} ,// =>Exit
-                    // 3.2 egistration for tracking personal pages
-                    {"2","noam","1"} //
+                    // 3.2 registration for tracking personal pages
+                    {"2","noam",null}, // =>There is no such page
+                    {"2","cUser",null},// =>You were successfully registered to the Team page
+                    //(3.3) Games alert notification
+                    {"3","80",null}, //=>There is no such game in the system
+                    {"3","1",null}, //=>You were registered successfully to the game alerts
+                    //(3.4) Insert a complaint
+                    {"4","I can't change my username on the system",null}, //=>Complaint added successfully
+                    {"4","",null}, //=>Complaint cannot be empty
+                    //(3.5) Viewing Search History
+                    {"5",null,null}, //=>no History
+                    //(3.5) Search
+                    {"7","fa",null}, //=>
+                    //(3.5) Viewing Search History
+                    {"5",null,null}, //=>have History
+                    //(3.6) Edit personal information
+                    {"6","1","shir cohen"}, //=>The name of the Subscription was successfully changed!
+                    {"6","2","shir0@"}, //=>The new email is not legal
+
             });
         }
 
@@ -47,16 +66,18 @@ public class FanMenuTest {
 
         @Test
         public void FanMenu1() {
-            Fan fan = new Fan("shir","12345","shir0@post.bgu.ac.il");
-            StartSystem sys = new StartSystem();
-            sys.startFromDB();
-            DataManagement.setCurrent(fan);
-            DataManagement.setSubscription(fan);
+            if(number.equals("1")){
+                StartSystem sys = new StartSystem();
+                sys.startFromDB();
+            }
+            DataManagement.setCurrent(DataManagement.containSubscription("shir"));
+            DataManagement.setSubscription(DataManagement.containSubscription("shir"));
 
             FanUserMenu FM = new FanUserMenu();
             ActionStatus ac =  FM.presentUserMenu(new String[]{number,arg1,arg2});
 
             System.out.print(ac.isActionSuccessful() + " " +ac.getDescription());
+
 
 
         }
--- src/Presentation_Layer/Users_Menu/FanUserMenu.java
+++ src/Presentation_Layer/Users_Menu/FanUserMenu.java
@@ -12,8 +12,8 @@ import java.util.Scanner;
 
 public class FanUserMenu implements UserMenu  {
 
-    private String fanMenu = "choose action: \n1:(3.1) Logout \n2:(3.2)Registration for tracking personal pages \n3:(3.3) Games alert notification" +
-            "\n4:(3.4) Writing a complaint \n5:(3.5) Viewing Search History \n6:(3.6) Edit personal information \n7:(3.5) Search \n8:Exit" ;
+    private String fanMenu = "choose action: \n1:(3.1) Logout \n2:(3.2) Registration for tracking personal pages \n3:(3.3) Games alert notification" +
+            "\n4:(3.4) Insert a complaint \n5:(3.5) Viewing Search History \n6:(3.6) Edit personal information \n7:(3.5) Search \n8:Exit" ;
 
 
 
@@ -40,54 +40,50 @@ public class FanUserMenu implements UserMenu  {
             if (input == 2) {
                 output += "insert username of the person you want to follow:  \nuser input- " + args[1] +"\n";
                 String username =args[1];
-                if(StartSystem.getAc().fanRegisterToPage(username).isActionSuccessful()){
-                    output+="The transaction completed successfully";
-                    return new ActionStatus(true,output);
-                }else{
-                    output += "The field you entered was incorrect.";
-                    return new ActionStatus(false,output);
-                }
+                ActionStatus ac = StartSystem.getAc().fanRegisterToPage(username);
+                return new ActionStatus(ac.isActionSuccessful(),output + ac.getDescription());
             }
         }
         else if(input ==3 ) {
-            output += "Enter a game id: " + args[1] +"\n";
+            output += "insert a game id: \nuser input-" + args[1] +"\n";
             int game =  Integer.parseInt(args[1]);
-            if(StartSystem.getAc().fanRegisterToGameAlerts(game).isActionSuccessful()){
-                return new ActionStatus(true,output + "The transaction completed successfully.");
-            }else{
-                return new ActionStatus(false,output+"The number you entered was incorrect.");
-            }
+            ActionStatus ac = StartSystem.getAc().fanRegisterToGameAlerts(game);
+            return new ActionStatus(ac.isActionSuccessful(),output + ac.getDescription());
         }
         else if(input ==4 ) {
-            String complaint =  args[1] +"\n";
-            return StartSystem.getAc().addComplaint(complaint);
+            output +=  "insert complaint: \nuser input- " + args[1] +"\n";
+            String complaint =  args[1];
+            ActionStatus ac = StartSystem.getAc().addComplaint(complaint);
+            return new ActionStatus(ac.isActionSuccessful(),output + ac.getDescription());
         }
         else if(input ==5 ) {
             return new ActionStatus(true ,output + StartSystem.getSc().showSearchHistory());
         }else if(input ==6 ) {
-            output += "choose action: \n1:Edit name \n2:Edit email" + args[1] + "\n";
+            output += "choose action: \n1:Edit name \n2:Edit email\n \nuser input- " + args[1] + "\n";
             int edit =  Integer.parseInt(args[1]);
             if(edit ==1 ){
-                output += "Write name:" + args[2] +"\n";
+                output += "insert name: \nuser input- " + args[2] +"\n";
                 String name =  args[2];
-                return new ActionStatus(true ,output + StartSystem.getESUDc().editSubscriptionName(DataManagement.getCurrent().getUserName(),name).getDescription());
+                ActionStatus ac = StartSystem.getESUDc().editSubscriptionName(DataManagement.getCurrent().getUserName(),name);
+                return new ActionStatus(ac.isActionSuccessful(),output + ac.getDescription());
             }else if(edit==2){
-                output += "Write emil:" + args[2] +"\n";
+                output += "insert emil: \nuser input- " + args[2] +"\n";
                 String name =  args[2];
-                return new ActionStatus(true ,output + StartSystem.getESUDc().editSubscriptionEmail(DataManagement.getCurrent().getUserName(),name).getDescription());
+                ActionStatus ac = StartSystem.getESUDc().editSubscriptionEmail(DataManagement.getCurrent().getUserName(),name);
+                return new ActionStatus(ac.isActionSuccessful(),output + ac.getDescription());
             }else{
                 return new ActionStatus(false ,output + "The digit is invalid.");
             }
         }
         else if(input==7) {
-            output += "Write Search word: " + args[1] + "\n";
+            output += "insert Search word: \nuser input- " + args[1] + "\n";
             String word = args[1];
             return new ActionStatus(true, output + StartSystem.getSc().findData(word));
         }
         else if(input ==8){
-            output += "insert User name: " + args[1] +"\n";
+            output += "insert User name: \nuser input- " + args[1] +"\n";
             String username =   args[1];
-            output += "insert password: " + args[2] +"\n";
+            output += "insert password: \nuser input- " + args[2] +"\n";
             String password =  args[2];
             ActionStatus ac = StartSystem.LEc.Exit(username,password);
             return new ActionStatus(ac.isActionSuccessful(),output + ac.getDescription());
