7d6c6b6a726543f0da32e51e4d01a87a725da5c4
school & work <michalez@post.bgu.ac.il>
2020-05-30 11:30:57 +0300

    remote connection working

--- src/DB_Layer/JDBC/DatabaseManagerMSSQLServer.java
+++ src/DB_Layer/JDBC/DatabaseManagerMSSQLServer.java
@@ -6,6 +6,7 @@ public class DatabaseManagerMSSQLServer extends DatabaseManager {
 
     public DatabaseManagerMSSQLServer(String databaseName) {
         super("jdbc:sqlserver://132.72.65.66;", databaseName, "sa", "123456789");
+        //super("jdbc:sqlserver://localhost;Instance=ISE-STR10;integratedSecurity=true", databaseName, "", "");
         //super("jdbc:sqlserver://localhost;integratedSecurity=true", databaseName, "", "");
     }
 
