3a63ac2657f227e69af30d10e1425b3ab7d92b09
razkl <tn5xjZhj>
2020-05-30 09:45:09 +0300

    commiting the test

--- src/BusinessService/Business_Layer/UserManagement/UnionRepresentative.java
+++ src/BusinessService/Business_Layer/UserManagement/UnionRepresentative.java
@@ -3,7 +3,7 @@ package BusinessService.Business_Layer.UserManagement;
 import java.util.Observable;
 import java.util.Observer;
 
-public class UnionRepresentative extends Subscription implements Observer {
+public class  UnionRepresentative extends Subscription implements Observer {
 
     public UnionRepresentative(String arg_user_name, String arg_password,String email) {
         super(arg_user_name, arg_password,email);
